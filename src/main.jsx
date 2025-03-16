25494cf3b6128050e853fa7fa2c120c3ae01f2ff
phatommobb <nickoluoch1130@gmail.com>
2025-03-16 12:26:13 +0300

    clean slate

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,16 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './routes/root';
-import ErrorPage from './error-page';
-import Contact from './routes/contact';
 
-createRoot(document.getElementById('root')).render(<StrictMode></StrictMode>);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
