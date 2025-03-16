f02b11b8f0236d00be9da96dd7a3a4b46e3167bf
phatommobb <nickoluoch1130@gmail.com>
2025-03-16 12:20:03 +0300

    clean slate

--- src/main.jsx
+++ src/main.jsx
@@ -6,22 +6,4 @@ import Root from './routes/root';
 import ErrorPage from './error-page';
 import Contact from './routes/contact';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: 'contacts/:contactId',
-        element: <Contact />,
-      },
-    ],
-  },
-]);
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+createRoot(document.getElementById('root')).render(<StrictMode></StrictMode>);
