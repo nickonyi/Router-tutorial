4b2c7f13b263859016137fcd187e6f99c021f8c5
phatommobb <nickoluoch1130@gmail.com>
2025-03-16 12:36:31 +0300

    contact component

---
+++ src/routes/contact.jsx
@@ -0,0 +1,12 @@
+import { Form } from 'react-router-dom';
+
+export default function Contact() {
+  const contact = {
+    first: 'Your',
+    last: 'Name',
+    avatar: 'https://robohash.org/you.png?size=200x200',
+    twitter: 'your_handle',
+    notes: 'Some notes',
+    favorite: true,
+  };
+}
