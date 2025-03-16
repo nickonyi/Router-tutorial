d5f8e1d3b37a70807ff00273277f18ae1ef47856
phatommobb <nickoluoch1130@gmail.com>
2025-03-16 12:51:13 +0300

    contact component

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
   },
   {
-    path: 'contacts:/contactId',
+    path: 'contacts/:contactId',
     element: <Contact />,
   },
 ]);
