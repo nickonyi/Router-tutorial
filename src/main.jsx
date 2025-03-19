5b46ada7778af1ae06d2e3f7d66106b1bfaacb69
phatommobb <nickoluoch1130@gmail.com>
2025-03-19 15:00:03 +0300

    react routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root, {
-  loader as rootLoader
-  , action as rootAction 
+  loader as rootLoader,
+  action as rootAction,
 } from './routes/root';
 import ErrorPage from './error-page';
-import Contact, { loader as contactLoader} from './routes/contact';
+import Contact, { loader as contactLoader } from './routes/contact';
 import EditContact from './routes/edit';
 
 const router = createBrowserRouter([
@@ -15,13 +15,11 @@ const router = createBrowserRouter([
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
-    loader:rootLoader,
-    action:rootAction,
+    loader: rootLoader,
     children: [
       {
         path: 'contacts/:contactId',
         element: <Contact />,
-        loader: contactLoader,
       },
       {
         path: 'contacts/:contactId/edit',
