7d0a3cb8df2f85fda94ea842447ce2127fb000f2
phatommobb <nickoluoch1130@gmail.com>
2025-03-20 14:50:09 +0300

    react router

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,12 @@
-import { Outlet, useLoaderData, Link, Form, redirect } from 'react-router-dom';
+import {
+  Outlet,
+  useLoaderData,
+  Link,
+  Form,
+  redirect,
+  NavLink,
+  useNavigation,
+} from 'react-router-dom';
 import { getContacts, createContact } from '../contact.js';
 import { Favorite } from './contact.jsx';
 
@@ -14,6 +22,7 @@ export async function action() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
 
   return (
     <>
@@ -40,7 +49,12 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? 'active' : isPending ? 'pending' : ''
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -49,7 +63,7 @@ export default function Root() {
                       <i>No name!</i>
                     )}{' '}
                     {contact.favorite && <Favorite contact={contact} />}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
@@ -60,7 +74,10 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id="detail">
+      <div
+        id="detail"
+        className={navigation.state === 'loading' ? 'loading' : ''}
+      >
         <Outlet />
       </div>
     </>
